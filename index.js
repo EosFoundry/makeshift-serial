95f7f12f3560d05b12c399c45b6170e3636e7179
James Liu <liujames.q@gmail.com>
2022-02-14 10:50:20 -0500

    initial work on serial read

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-const { fork } = require('child_process');
-const lsReturnData = fork(__dirname + '/childProcess.js');
+// const { fork } = require('child_process');
+// const lsReturnData = fork(__dirname + '/childProcess.js');
 
-lsReturnData.on('message', (m) => {
-    console.log('message received');
-    console.log(m);
+// lsReturnData.on('message', (m) => {
+//     console.log('message received');
+//     console.log(m);
+// })
+
+// setTimeout(() => { console.log('child ended'), 4999})
+
+
+const { SerialPort } = require('serialport');
+
+const port = new SerialPort({
+    path: 'COM3',
+    baudRate: 9600,
 })
 
-setTimeout(() => { console.log('child ended'), 5000})
+// Open errors will be emitted as an error event
+port.on('error', function (err) {
+    console.log('Error: ', err.message)
+})
+
+
+// Switches the port into "flowing mode"
+port.on('data', function (data) {
+    console.log('Data:', data)
+})
+
+port.write('main screen turn on', function (err) {
+    if (err) {
+        return console.log('Error on write: ', err.message)
+    }
+    console.log('message written')
+})
+
+
+setInterval( () => {
+        port.write('Hi Mom!')
+    },
+    1000
+)
